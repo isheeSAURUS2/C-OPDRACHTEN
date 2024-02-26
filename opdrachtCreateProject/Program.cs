652e1c2c20af3556046b2ac85847e7a239d342d6
julien oving <37832@ma-web.nl>
2024-02-26 14:39:55 +0100

    initial

---
+++ opdrachtCreateProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace opdrachtCreateProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dit is die ene opdracht waar je een nieuw project moest maken");
+        }
+    }
+}
